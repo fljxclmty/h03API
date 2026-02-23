6b02f4e745f94c5565c88df2ff7af8471e4d81a3
Anton Ilchenko <anton56.234@gmail.com>
2026-02-23 12:00:01 +0500

    edited settings

--- src/core/settings.ts
+++ src/core/settings.ts
@@ -1,8 +1,11 @@
 export const SETTINGS = {
+    // Vercel сам назначит PORT, поэтому приоритет отдаем переменной окружения
     PORT: process.env.PORT || 5003,
-    MONGO_URL:
-        process.env.MONGO_URL ||
-        'mongodb+srv://fljx:DMtDKmIa9Flan8h5@cluster0.kkaodex.mongodb.net/home-task-db?retryWrites=true&w=majority&appName=Cluster0',
 
+    // В Vercel будет использоваться значение из настроек проекта,
+    // а локально — твоя строка (но лучше и локально использовать .env файл)
+    MONGO_URL: process.env.MONGO_URL || 'mongodb+srv://fljx:DMtDKmIa9Flan8h5@cluster0.kkaodex.mongodb.net/home-task-db?retryWrites=true&w=majority&appName=Cluster0',
+
+    // Убедись, что имя базы совпадает с тем, что ты используешь в коде
     DB_NAME: process.env.DB_NAME || 'h03_database',
 };
