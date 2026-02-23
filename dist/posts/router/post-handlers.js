457f31cc4e33e0cc40af7f58840b72c0b3367004
Anton Ilchenko <anton56.234@gmail.com>
2026-02-23 12:04:22 +0500

    edited post handlers

--- dist/posts/router/post-handlers.js
+++ dist/posts/router/post-handlers.js
@@ -12,103 +12,49 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.deletePostHandler = exports.updatePostHandler = exports.createPostHandler = exports.getPostByIdHandler = exports.getPostsHandler = void 0;
 const posts_repository_1 = require("../repositories/posts-repository");
 const statuses_1 = require("../../core/statuses");
-// 1. Получение всех постов
-function getPostsHandler(req, res) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const posts = yield posts_repository_1.postsRepository.getAll();
-            res.status(statuses_1.HttpStatus.Ok).send(posts);
-        }
-        catch (e) {
-            console.error(e);
-            res.status(statuses_1.HttpStatus.InternalServerError).send({ message: "Internal server error" });
-        }
-    });
-}
+const getPostsHandler = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const posts = yield posts_repository_1.postsRepository.getAll();
+    res.status(statuses_1.HttpStatus.Ok).send(posts);
+});
 exports.getPostsHandler = getPostsHandler;
-// 2. Получение поста по ID
-function getPostByIdHandler(req, res) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const post = yield posts_repository_1.postsRepository.getById(req.params.id);
-            if (post) {
-                res.status(statuses_1.HttpStatus.Ok).send(post);
-            }
-            else {
-                res.sendStatus(statuses_1.HttpStatus.NotFound);
-            }
-        }
-        catch (e) {
-            console.error(e);
-            res.status(statuses_1.HttpStatus.InternalServerError).send({ message: "Internal server error" });
-        }
-    });
-}
+const getPostByIdHandler = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const post = yield posts_repository_1.postsRepository.getById(req.params.id);
+    if (post) {
+        res.status(statuses_1.HttpStatus.Ok).send(post);
+    }
+    else {
+        res.sendStatus(statuses_1.HttpStatus.NotFound);
+    }
+});
 exports.getPostByIdHandler = getPostByIdHandler;
-// 3. Создание поста
-function createPostHandler(req, res) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const newPostData = req.body;
-            const createdPost = yield posts_repository_1.postsRepository.createPost(newPostData);
-            if (createdPost) {
-                // Если пост создан успешно (блог найден и ID сгенерирован)
-                res.status(statuses_1.HttpStatus.Created).send(createdPost);
-            }
-            else {
-                // Если postsRepository вернул null (например, указанный blogId не существует)
-                res.status(statuses_1.HttpStatus.BadRequest).send({
-                    errorsMessages: [
-                        { message: "Blog with specified blogId not found", field: "blogId" }
-                    ]
-                });
-            }
-        }
-        catch (e) {
-            console.error(e);
-            res.status(statuses_1.HttpStatus.InternalServerError).send({ message: "Internal server error" });
-        }
-    });
-}
+const createPostHandler = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const newPost = yield posts_repository_1.postsRepository.createPost(req.body);
+    if (newPost) {
+        res.status(statuses_1.HttpStatus.Created).send(newPost);
+    }
+    else {
+        // Если blogId не валиден, возвращаем 400
+        res.sendStatus(statuses_1.HttpStatus.BadRequest);
+    }
+});
 exports.createPostHandler = createPostHandler;
-// 4. Обновление поста
-function updatePostHandler(req, res) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const id = req.params.id;
-            const updateData = req.body;
-            const isUpdated = yield posts_repository_1.postsRepository.updatePost(id, updateData);
-            if (isUpdated) {
-                res.sendStatus(statuses_1.HttpStatus.NoContent);
-            }
-            else {
-                // Либо пост не найден, либо передан несуществующий blogId
-                res.sendStatus(statuses_1.HttpStatus.NotFound);
-            }
-        }
-        catch (e) {
-            console.error(e);
-            res.status(statuses_1.HttpStatus.InternalServerError).send({ message: "Internal server error" });
-        }
-    });
-}
+const updatePostHandler = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const isUpdated = yield posts_repository_1.postsRepository.updatePost(req.params.id, req.body);
+    if (isUpdated) {
+        res.sendStatus(statuses_1.HttpStatus.NoContent);
+    }
+    else {
+        res.sendStatus(statuses_1.HttpStatus.NotFound);
+    }
+});
 exports.updatePostHandler = updatePostHandler;
-// 5. Удаление поста
-function deletePostHandler(req, res) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const isDeleted = yield posts_repository_1.postsRepository.deletePost(req.params.id);
-            if (isDeleted) {
-                res.sendStatus(statuses_1.HttpStatus.NoContent);
-            }
-            else {
-                res.sendStatus(statuses_1.HttpStatus.NotFound);
-            }
-        }
-        catch (e) {
-            console.error(e);
-            res.status(statuses_1.HttpStatus.InternalServerError).send({ message: "Internal server error" });
-        }
-    });
-}
+const deletePostHandler = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const isDeleted = yield posts_repository_1.postsRepository.deletePost(req.params.id);
+    if (isDeleted) {
+        res.sendStatus(statuses_1.HttpStatus.NoContent);
+    }
+    else {
+        res.sendStatus(statuses_1.HttpStatus.NotFound);
+    }
+});
 exports.deletePostHandler = deletePostHandler;
--- src/posts/router/post-handlers.ts
+++ src/posts/router/post-handlers.ts
@@ -1,95 +1,62 @@
-import { Request, Response } from "express";
-import { postsRepository } from "../repositories/posts-repository";
-import {HttpStatus} from "../../core/statuses";
-import {PostInputModel} from "../schemas/post-input-model";
+import { Request, Response } from 'express';
+import {postsRepository} from "../repositories/posts-repository";
+import { PostInputModel } from '../schemas/post-input-model';
+import { PostViewModel } from '../schemas/post-view-model';
+import { HttpStatus } from '../../core/statuses';
 
+export const getPostsHandler = async (
+    req: Request,
+    res: Response<PostViewModel[]>
+) => {
+    const posts = await postsRepository.getAll();
+    res.status(HttpStatus.Ok).send(posts);
+};
 
-
-
-// 1. Получение всех постов
-
-
-export async function getPostsHandler(req: Request, res: Response) {
-    try {
-        const posts = await postsRepository.getAll();
-        res.status(HttpStatus.Ok).send(posts);
-    } catch (e) {
-        console.error(e);
-        res.status(HttpStatus.InternalServerError).send({ message: "Internal server error" });
+export const getPostByIdHandler = async (
+    req: Request<{ id: string }>,
+    res: Response<PostViewModel>
+) => {
+    const post = await postsRepository.getById(req.params.id);
+    if (post) {
+        res.status(HttpStatus.Ok).send(post);
+    } else {
+        res.sendStatus(HttpStatus.NotFound);
     }
-}
+};
 
-// 2. Получение поста по ID
-export async function getPostByIdHandler(req: Request, res: Response) {
-    try {
-        const post = await postsRepository.getById(req.params.id);
-
-        if (post) {
-            res.status(HttpStatus.Ok).send(post);
-        } else {
-            res.sendStatus(HttpStatus.NotFound);
-        }
-    } catch (e) {
-        console.error(e);
-        res.status(HttpStatus.InternalServerError).send({ message: "Internal server error" });
+export const createPostHandler = async (
+    req: Request<{}, {}, PostInputModel>,
+    res: Response<PostViewModel>
+) => {
+    const newPost = await postsRepository.createPost(req.body);
+    if (newPost) {
+        res.status(HttpStatus.Created).send(newPost);
+    } else {
+        // Если blogId не валиден, возвращаем 400
+        res.sendStatus(HttpStatus.BadRequest);
     }
-}
+};
 
-// 3. Создание поста
-export async function createPostHandler(req: Request, res: Response) {
-    try {
-        const newPostData: PostInputModel = req.body;
-        const createdPost = await postsRepository.createPost(newPostData);
-
-        if (createdPost) {
-            // Если пост создан успешно (блог найден и ID сгенерирован)
-            res.status(HttpStatus.Created).send(createdPost);
-        } else {
-            // Если postsRepository вернул null (например, указанный blogId не существует)
-            res.status(HttpStatus.BadRequest).send({
-                errorsMessages: [
-                    { message: "Blog with specified blogId not found", field: "blogId" }
-                ]
-            });
-        }
-    } catch (e) {
-        console.error(e);
-        res.status(HttpStatus.InternalServerError).send({ message: "Internal server error" });
+export const updatePostHandler = async (
+    req: Request<{ id: string }, {}, PostInputModel>,
+    res: Response
+) => {
+    const isUpdated = await postsRepository.updatePost(req.params.id, req.body);
+    if (isUpdated) {
+        res.sendStatus(HttpStatus.NoContent);
+    } else {
+        res.sendStatus(HttpStatus.NotFound);
     }
-}
+};
 
-// 4. Обновление поста
-export async function updatePostHandler(req: Request, res: Response) {
-    try {
-        const id = req.params.id;
-        const updateData: PostInputModel = req.body;
-
-        const isUpdated = await postsRepository.updatePost(id, updateData);
-
-        if (isUpdated) {
-            res.sendStatus(HttpStatus.NoContent);
-        } else {
-            // Либо пост не найден, либо передан несуществующий blogId
-            res.sendStatus(HttpStatus.NotFound);
-        }
-    } catch (e) {
-        console.error(e);
-        res.status(HttpStatus.InternalServerError).send({ message: "Internal server error" });
+export const deletePostHandler = async (
+    req: Request<{ id: string }>,
+    res: Response
+) => {
+    const isDeleted = await postsRepository.deletePost(req.params.id);
+    if (isDeleted) {
+        res.sendStatus(HttpStatus.NoContent);
+    } else {
+        res.sendStatus(HttpStatus.NotFound);
     }
-}
-
-// 5. Удаление поста
-export async function deletePostHandler(req: Request, res: Response) {
-    try {
-        const isDeleted = await postsRepository.deletePost(req.params.id);
-
-        if (isDeleted) {
-            res.sendStatus(HttpStatus.NoContent);
-        } else {
-            res.sendStatus(HttpStatus.NotFound);
-        }
-    } catch (e) {
-        console.error(e);
-        res.status(HttpStatus.InternalServerError).send({ message: "Internal server error" });
-    }
-}
+};
